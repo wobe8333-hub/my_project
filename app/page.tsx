d723c85c66d99fa787e48e04f131489ed42250c6
wobe8333 <wobe8333@gmail.com>
2026-07-27 15:29:25 +0900

    chore: 프로젝트 초기 세팅

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+export default function HomePage() {
+  return (
+    <main style={{ padding: 24 }}>
+      <h1>초개인화 헬스 트레이닝</h1>
+      <p>
+        <a href="/login">로그인</a> · <a href="/signup">회원가입</a>
+      </p>
+    </main>
+  )
+}
